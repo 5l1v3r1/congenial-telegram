4ba2c3ae8c4430b6755a2d1ed47034dbba3a95e3
cyraxz <30682066+cyraxz@users.noreply.github.com>
2020-06-04 02:05:50 -0400

    Update systemchecker.sh

--- systemchecker.sh
+++ systemchecker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-host -t CNAME www.google.com | grep 'alias for' | awk '{print $NF}' | echo &> dangling 
+host -t CNAME  | grep 'alias for' | awk '{print $NF}' | echo &> dangling 
 echo $?
